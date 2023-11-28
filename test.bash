4da83e95b4d29c6c432525397764c2b273134bf1
Keigo Shishido <s22c1065nb@s.chibakoudai.jp>
2023-11-28 18:21:09 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -8,15 +8,12 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
-       
+[ "${out}" = 15.0 ] || ng ${LINENO}       
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-    　
+[ "${out}" = 15 ] || ng ${LINENO}    　
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-    　
+[ "${out}" = "" ] || ng ${LINENO}    　
 out=$(echo | ./plus) 
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
