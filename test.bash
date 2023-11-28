7b6166ba8490df82b195dde63c24206113fa1ac5
Keigo Shishido <s22c1065nb@s.chibakoudai.jp>
2023-11-28 17:50:55 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Keigo Shishido
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
@@ -10,5 +10,18 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+[ "$res" = 0 ] && echo OK     
+  
+### I/O ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+    　
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+    　
+out=$(echo | ./plus) 
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 exit $res
