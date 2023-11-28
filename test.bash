16f3da213dba592a7d7448c27f51e2c48bd9ae69
Keigo Shishido <s22c1065nb@s.chibakoudai.jp>
2023-11-28 18:12:53 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -7,17 +7,12 @@ ng () {
 }
 res=0
 
-### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK     
-  
-### I/O ###
+       
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
     　
-### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
@@ -25,4 +20,7 @@ out=$(echo あ | ./plus)
 out=$(echo | ./plus) 
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
 exit $res
